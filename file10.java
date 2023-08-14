57a2c2d9196e6b8854bc9dd580696ebf5fa4bb7b
Hareesh76 <127645465+Hareesh76@users.noreply.github.com>
2023-08-14 21:42:55 +0530

    Add files via upload

---
+++ file10.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+ 
+public class file10 {
+     public static void main(String a[]){
+        StringBuilder sb = new StringBuilder();
+        String strLine = "";
+        try
+          {
+             String filename= "D:/file/test.txt";
+             FileWriter fw = new FileWriter(filename,true); 
+             //appends the string to the file
+             fw.write("Java Exercises\n");
+             fw.close();
+             BufferedReader br = new BufferedReader(new FileReader("D:/file/test.txt"));
+             //read the file content
+             while (strLine != null)
+             {
+                sb.append(strLine);
+                sb.append(System.lineSeparator());
+                strLine = br.readLine();
+                System.out.println(strLine);
+            }
+             br.close();
+           }
+           catch(IOException ioe)
+           {
+            System.err.println("IOException: " + ioe.getMessage());
+           }
+        }
+  }
+
